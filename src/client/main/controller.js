4ee952be32c0c5b8e49b18afd7ebd511ce555bcc
designeng <denissavenok@gmail.com>
2016-03-29 10:25:04 +0300

    postsContainer

--- src/client/main/controller.js
+++ src/client/main/controller.js
@@ -2,16 +2,18 @@ import $ from 'jquery';
 import _ from 'underscore';
 
 function controller() {
+    _.bindAll(this, ['onReady']);
+    this.postsContainer = $('#scroller').find('ul');
 }
 
 controller.prototype.onReady = function(additionalPosts, invocationEnvironment) {
-    $(window).scroll(function() {
+    $(window).scroll(() => {
         if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
             _.extend(invocationEnvironment, {
                 fromPostId: window.__sharedData__.lastPostId
             });
             additionalPosts.call(null, invocationEnvironment).then(context => {
-                $('#scroller').find('ul').append(context.postsBlock);
+                this.postsContainer.append(context.postsBlock);
                 window.__sharedData__.lastPostId = _.last(context.transformedPosts).Id;
             })
         }
