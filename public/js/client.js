2872921e234217d0ba549b9c98ff1d41c04e1a29
designeng <denissavenok@gmail.com>
2016-03-29 11:03:46 +0300

    magnificPopup initialization script reverted to post tpl

--- public/js/client.js
+++ public/js/client.js
@@ -16,25 +16,4 @@
     logo.on('click', function(){
         window.location = '/';
     });
-
-    var zooms = $('a[data-id=zoom]');
-    var zoomsLength = zooms.length;
-    for (var i = zoomsLength - 1; i >= 0; i--) {
-        $(zooms[i]).magnificPopup({
-            type:'image',
-            midClick: true,
-            closeOnContentClick: true,
-            showCloseBtn: false,
-            zoom: {
-                enabled: true,
-                duration: 400,
-                easing: 'ease-in-out',
-                // TODO: find how to fix it. 
-                // Fisrt time element is clicked it's starting not from openerElement shape.
-                opener: function(openerElement) {
-                    return openerElement;
-                }
-            }
-        });
-    };
 })();
