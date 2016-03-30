89b126215ba9d76d78767b2301506d31be6296e3
designeng <denissavenok@gmail.com>
2016-03-30 19:16:20 +0300

    joined post channels captions

--- src/blocks/body/getBodyHtml.js
+++ src/blocks/body/getBodyHtml.js
@@ -1,22 +1,33 @@
 import _ from 'underscore';
 
-function correctChannel(channels, channel) {
+function correctChannel(channels, channel, postId, lastPost) {
     let _channel = _.find(channels, {Id: channel.id});
     if(_channel) {
         channel = { 
-            name: _channel['Caption'],
-            id: _channel['Id'],
+            name    : _channel.Caption,
+            id      : _channel.Id,
         };
+    }
+
+    // in case of single post and post page
+    if(postId) {
+        let _channels = []
+        _.each(lastPost.ChannelIds, (Id) => {
+            let channel = _.find(channels, { Id });
+            if(channel) {
+                _channels.push(channel.Caption);
+            }
+        })
+        channel.name = _channels.join(", ")
     }
     return channel;
 }
 
 export default function getBodyHtml(postsBlock, getCarcassFn, posts, channels, channel, postId) {
-    
-    // correct channel name
-    channel = correctChannel(channels, channel);
+    let lastPost = posts[posts.length - 1];
+    channel = correctChannel(channels, channel, postId, lastPost);
 
-    let lastPostId = posts[posts.length - 1].Id;
+    let lastPostId = lastPost.Id;
     let sharedData = { lastPostId, channels, channel, postId };
     
     const pageHtml = getCarcassFn(postsBlock, channel, sharedData);
