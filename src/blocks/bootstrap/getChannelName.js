da7e99eb0392de82c7fdc679e664dff7624709bf
designeng <denissavenok@gmail.com>
2016-03-30 18:38:15 +0300

    src/blocks/bootstrap/transformChannels.js removed

--- src/blocks/bootstrap/getChannelName.js
+++
@@ -1,6 +0,0 @@
-import _ from 'underscore';
-
-export default function getChannelName(channelId, channels, defaultCaption) {
-    let channel = _.find(channels, {Id: channelId});
-    return channel ? channel['Caption'] : defaultCaption;
-}
