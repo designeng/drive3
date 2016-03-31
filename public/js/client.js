2fa97bb4a9e72fff6fcd2cf1539d819337d51d35
designeng <denissavenok@gmail.com>
2016-03-31 16:01:57 +0300

    toggle rotate added (not work)

--- public/js/client.js
+++ public/js/client.js
@@ -2,10 +2,12 @@
     var channelsMenu = $('.channels-menu');
     var logo = $('#logo');
     var channelsMenuToggler = $('.menu-trigger-link');
+    var channelsMenuTogglerSign = $('.toggler-sign');
 
     function toggleMenu() {
         channelsMenu.toggleClass('opened');
         channelsMenuToggler.toggleClass('toggler-opened');
+        channelsMenuTogglerSign.toggleClass('toggler-sign-opened');
     }
 
     channelsMenuToggler.on('click', function(e){
