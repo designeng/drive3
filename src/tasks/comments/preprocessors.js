0e6c087436f297fabcdc3e57cc5d83a20f258aad
designeng <denissavenok@gmail.com>
2016-04-05 20:32:11 +0300

    twemojiParse array arg; AuthorAvatar first item

--- src/tasks/comments/preprocessors.js
+++ src/tasks/comments/preprocessors.js
@@ -10,7 +10,7 @@ export function prepareComments(commentsData) {
     return _.map(comments, (comment) => {
         const authorProfile = _.find(profiles, {Id: comment.AuthorId});
         return {
-            AuthorAvatar: authorProfile.Avatar[0].Url,
+            AuthorAvatar: authorProfile.Avatar[1].Url,
             AuthorNickname: authorProfile.Nickname,
             CreatedOn: moment(comment.CreatedOn).fromNow(),
             Content: comment.Content
