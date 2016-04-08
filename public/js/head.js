ca7e219d1ec40d7279c43cf80fa1fb635b0dfda1
designeng <denissavenok@gmail.com>
2016-04-08 18:45:50 +0300

    head script align

--- public/js/head.js
+++ public/js/head.js
@@ -1,50 +1,47 @@
-
-    var createMagnificPopup = function(selector) {
-        $(selector).magnificPopup({
-            type:'image',
-            midClick: true,
-            closeOnContentClick: true,
-            showCloseBtn: false,
-            zoom: {
-                enabled: true,
-                duration: 400,
-                easing: 'ease-in-out',
-                // TODO: find how to fix it. 
-                // Fisrt time element is clicked it's starting not from openerElement shape.
-                opener: function(openerElement) {
-                    return openerElement;
-                }
+var createMagnificPopup = function(selector) {
+    $(selector).magnificPopup({
+        type:'image',
+        midClick: true,
+        closeOnContentClick: true,
+        showCloseBtn: false,
+        zoom: {
+            enabled: true,
+            duration: 400,
+            easing: 'ease-in-out',
+            opener: function(openerElement) {
+                return openerElement;
             }
-        });
-    }
+        }
+    });
+}
 
-    var twemojiParse = function() {
-        var args = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments));
-        for (var i = args.length - 1; i >= 0; i--) {
-            var id = args[i];
-            twemoji.parse(document.getElementById(id));
+var twemojiParse = function() {
+    var args = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments));
+    for (var i = args.length - 1; i >= 0; i--) {
+        var id = args[i];
+        twemoji.parse(document.getElementById(id));
+    };
+}
+
+// TODO: it's only a row prototype.
+var showVotingResultsOnClick = function(id) {
+    var blockRootElement = $('#' + id);
+
+    var votingBarElements = blockRootElement.find('.post-voting-bar');
+    var percents = [];
+    for (var i = votingBarElements.length - 1; i >= 0; i--) {
+        percents.push(votingBarElements[i].style.width);
+        votingBarElements[i].style.width = "0";
+    };
+
+    blockRootElement.one('click', function() {
+        var votingOptionElements = blockRootElement.find('.post-voting-option');
+        var votingCaptionTeaserElements = blockRootElement.find('.post-voting-caption-teaser');
+
+        for (var i = votingOptionElements.length - 1; i >= 0; i--) {
+            $(votingOptionElements[i]).removeClass('post-voting-option-closed');
+            $(votingCaptionTeaserElements[i]).hide();
+            votingBarElements[i].style.width = percents[i];
         };
-    }
-
-    // TODO: it's only a row prototype.
-    var showVotingResultsOnClick = function(id) {
-        var blockRootElement = $('#' + id);
-
-        var votingBarElements = blockRootElement.find('.post-voting-bar');
-        var percents = [];
-        for (var i = votingBarElements.length - 1; i >= 0; i--) {
-            percents.push(votingBarElements[i].style.width);
-            votingBarElements[i].style.width = "0";
-        };
-
-        blockRootElement.one('click', function() {
-            var votingOptionElements = blockRootElement.find('.post-voting-option');
-            var votingCaptionTeaserElements = blockRootElement.find('.post-voting-caption-teaser');
-
-            for (var i = votingOptionElements.length - 1; i >= 0; i--) {
-                $(votingOptionElements[i]).removeClass('post-voting-option-closed');
-                $(votingCaptionTeaserElements[i]).hide();
-                votingBarElements[i].style.width = percents[i];
-            };
-        });
-    }
+    });
+}
