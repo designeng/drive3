836b567da6c52056717d1b1b65f5c2fea78522ec
designeng <denissavenok@gmail.com>
2016-04-14 20:45:27 +0300

    fixed

--- src/client/main/header/notification.js
+++ src/client/main/header/notification.js
@@ -4,9 +4,11 @@ export default function notification() {
     this.notification = $('.notification');
     this.notificationCloseButton = $('.notification-close-button');
 
-    console.log("notificationCloseButton", this.notificationCloseButton);
-
     this.notificationCloseButton.on('click', (e) => {
         this.notification.hide();
     });
+}
+
+notification.prototype.doSmth = function() {
+    
 }
