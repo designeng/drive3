120a48958a70a3f86137bfc5edfcea0d34731831
designeng <denissavenok@gmail.com>
2016-04-14 20:44:24 +0300

    fixed errors in notification

--- src/client/main/header/notification.js
+++ src/client/main/header/notification.js
@@ -1,18 +1,12 @@
 import $ from 'jquery';
 
 export default function notification() {
-    this.notification = $('.notification);
-    this.notificationCloseButton = $('notification-close-button');
+    this.notification = $('.notification');
+    this.notificationCloseButton = $('.notification-close-button');
 
-    console.log("this.notificationCloseButton", this.notificationCloseButton);
+    console.log("notificationCloseButton", this.notificationCloseButton);
 
     this.notificationCloseButton.on('click', (e) => {
-        console.log("clicked");
-        this.notification.hide();
-    });
-
-    this.notification.on('click', (e) => {
-        console.log("clicked notification");
         this.notification.hide();
     });
 }
--- src/client/main/header/spec.js
+++ src/client/main/header/spec.js
@@ -1,6 +1,6 @@
 import wireDebugPlugin from 'essential-wire/source/debug';
 import menuController  from './controller';
-import notification  from './controller';
+import notification    from './notification';
 
 export default {
     $plugins: [
