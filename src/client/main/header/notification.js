bcdad5e7e8404aaf5dc3eb4b5c4163292f480a6e
designeng <denissavenok@gmail.com>
2016-04-15 14:33:56 +0300

    extraPost.hbs removed; invocationEnvironment moved to zeroSpec

--- src/client/main/header/notification.js
+++ src/client/main/header/notification.js
@@ -5,12 +5,12 @@ export default function notification() {
     this.postsWrapper = $('#posts-wrapper');
 }
 
-notification.prototype.displayNotificationBlock = function(notificationBlock) {
+notification.prototype.displayNotificationBlock = function(notificationBlock, invocationEnvironment) {
     let displayCountLimit = 3;
     let count = parseInt(localStorage.getItem('extra-notification-count'));
     if(isNaN(count)) count = 0;
 
-    if(!count || count < displayCountLimit) {
+    if(!count || count < displayCountLimit && !invocationEnvironment.postId) {
         let topCorrectionClass = 'content-top-correction';
         this.postsWrapper.addClass(topCorrectionClass);
 
--- src/client/main/header/spec.js
+++ src/client/main/header/spec.js
@@ -26,7 +26,8 @@ export default {
         },
         ready: {
             displayNotificationBlock: [
-                {$ref: 'notificationBlock'}
+                {$ref: 'notificationBlock'},
+                {$ref: 'invocationEnvironment'}
             ]
         }
     }
